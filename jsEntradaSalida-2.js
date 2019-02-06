5d94e7cb6b4b53e50795113126e5e4b7eac4317d
Alejandra Escubilla Texis <47353421+AleEscubillaTexis@users.noreply.github.com>
2019-02-05 23:19:30 -0300

    Add files via upload

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,11 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+	var nombreApellidoDelAlumno;
+
+	nombreApellidoDelAlumno = prompt ("Por favor, ingrese su nombre y apellido");
+	alert("Su nombre es: " + nombreApellidoDelAlumno);
+
+}
+
---
+++ jsEntradaSalida-6.js
@@ -0,0 +1,24 @@
+/*Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt) y Sumarlos.
+mostrar el resulto por medio de "ALERT"*/
+function sumar()
+{
+	var numero1;
+	var numero2;
+	var resultado;
+
+
+	numero1 = numeroUno.value;
+	numero2 = numeroDos.value;
+
+	//parseInt(numero1);
+	//parseInt(numero2);
+	//(numero1 + numero2) = resultado;
+	
+	resultado = parseInt(numero1,10)+parseInt(numero2,10);
+	
+	alert("El resultado es: " + parseInt(resultado,10));
+
+
+}
+
