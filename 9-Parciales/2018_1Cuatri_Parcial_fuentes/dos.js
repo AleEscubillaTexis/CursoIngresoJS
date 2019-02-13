3928a1242aaae012330c43f86e22943e8e45fb1b
Alejandra Escubilla Texis <47353421+AleEscubillaTexis@users.noreply.github.com>
2019-02-13 09:30:36 -0300

    parcial 2 (13/02)

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,14 +1,22 @@
 function mostrar()
 {
   
-  var nombre;
-  var localidad;
+  	var precio;
+ 	var recargo;
+ 	var precioFinal;
 
+ 	precio = elNombre.value;
 
-  nombre = elNombre.value;
-  localidad = laLocalidad.value;
+ 	precio = parseInt(precio);
 
-  alert("Usted es " + nombre + " y vive en la localidad de "  + localidad)
+ 	recargo = ((precio * 10) /100);
+ 	console.log(recargo);
+
+ 	precioFinal = (precio + recargo)
+ 	console.log(precioFinal); 
+
+	alert("El precio del producto en efectivo es: $" + precio + ", con tarjeta tiene un recargo del 10% que seria $" + recargo + " de recargo, costandole final $ " + precioFinal);
+
 
 
 }
