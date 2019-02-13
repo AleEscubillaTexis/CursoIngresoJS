140985859da5d764300928c31af7d5adc5eba5b0
Alejandra Escubilla Texis <47353421+AleEscubillaTexis@users.noreply.github.com>
2019-02-13 09:12:54 -0300

    parcial 1 (13/02)

---
+++ 9-Parciales/uno.js
@@ -0,0 +1,26 @@
+
+function mostrar()
+{
+	var nombre1;
+	var nombre2;
+	var edad1;
+	var edad2;
+	var suma;
+
+	nombre1 = prompt("Ingrese su nombre");
+	nombre2 = prompt("Ingrese su nombre");
+	edad1 = prompt("Ingrese su edad");
+	edad2 = prompt("Ingrese su edad");
+
+	edad1 = parseInt(edad1);
+	edad2 = parseInt(edad2);
+
+	suma = (edad1 + edad2);
+
+	alert("Ustedes son " + nombre1 + " y " + nombre2 + ", sus edades son " + edad1 + " y " + edad2 + ", y la suma de sus edades es " + suma);
+
+
+	
+
+
+}
