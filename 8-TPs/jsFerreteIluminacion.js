9055e2a2e16ce25aedff0c9e1dd944a469bc6adf
Alejandra Escubilla Texis <47353421+AleEscubillaTexis@users.noreply.github.com>
2019-02-16 13:07:26 -0300

    tp (16/02)
    (clase)
    fijarse los if y else

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -26,59 +26,78 @@ function CalcularPrecio ()
 	precio = 35;
 
 	precio = (precio * cantidad);
+	console.log(precio);
 
  	if (cantidad >= 6)
  	{
  		descuento = ((precio * 50) / 100);
+ 		console.log(descuento);
  		precioFinal = (precio - descuento);
- 	}
-
- 	if ((cantidad == 5) && (marcaLampara == "ArgentinaLuz"))
- 	{
- 		descuento = ((precio * 40) / 100);
- 		precioFinal = (precio - descuento);
- 	}
- 	else
- 		{
- 			if (marcaLampara =! "ArgentinaLuz")
- 			{
- 				descuento = ((precio * 30) / 100);
- 				precioFinal = (precio - descuento);
- 			}
- 		}
-
- 	if ((cantidad == 4) && ((marcaLampara == "ArgentinaLuz") || (marcaLampara == "FelipeLamparas")))
- 	{
- 		descuento = ((precio * 25) / 100);
- 		precioFinal = (precio - descuento);
+ 		console.log(precioFinal);
  	}
  	else
  	{
- 		if ((marcaLampara =! "ArgentinaLuz") && (marcaLampara =! "FelipeLamparas"))
- 		{
- 			descuento = ((precio * 20) / 100);
- 			precioFinal = (precio - descuento);
- 		}
- 	}
+	 	if ((cantidad == 5) && (marcaLampara == "ArgentinaLuz"))
+	 	{
+	 		descuento = ((precio * 40) / 100);
+	 		console.log(descuento);
+	 		precioFinal = (precio - descuento);
+	 		console.log(precioFinal);
+	 	}
+	 	else
+	 		{
+	 			if (marcaLampara =! "ArgentinaLuz")
+	 			{
+	 				descuento = ((precio * 30) / 100);
+	 				console.log(descuento);
+	 				precioFinal = (precio - descuento);
+	 				console.log(precioFinal);
+	 			}
+	 		}
 
- 	if ((cantidad == 3) && (marcaLampara == "ArgentinaLuz"))
- 	{
- 		descuento = ((precio * 15) / 100);
- 		precioFinal = (precio - descuento);
- 	}
- 	else
- 	{
- 		if (marcaLampara == "FelipeLamparas")
- 		{
- 			descuento = ((precio * 10) / 100);
- 			precioFinal = (precio - descuento);
- 		}
- 		else
- 		{
- 			descuento = ((precio * 5) / 100);
- 			precioFinal = (precio - descuento);
- 		}
- 	}
+	 	if ((cantidad == 4) && ((marcaLampara == "ArgentinaLuz") || (marcaLampara == "FelipeLamparas")))
+	 	{
+	 		descuento = ((precio * 25) / 100);
+	 		console.log(descuento);
+	 		precioFinal = (precio - descuento);
+	 		console.log(precioFinal);
+	 	}
+	 	else
+	 	{
+	 		if ((marcaLampara =! "ArgentinaLuz") && (marcaLampara =! "FelipeLamparas"))
+	 		{
+	 			descuento = ((precio * 20) / 100);
+	 			console.log(descuento);
+	 			precioFinal = (precio - descuento);
+	 			console.log(precioFinal);
+	 		}
+	 	}
+
+	 	if ((cantidad == 3) && (marcaLampara == "ArgentinaLuz"))
+	 	{
+	 		descuento = ((precio * 15) / 100);
+	 		console.log(descuento);
+	 		precioFinal = (precio - descuento);
+	 		console.log(precioFinal);
+	 	}
+	 	else
+	 	{
+	 		if (marcaLampara == "FelipeLamparas")
+	 		{
+	 			descuento = ((precio * 10) / 100);
+	 			console.log(descuento);
+	 			precioFinal = (precio - descuento);
+	 			console.log(precioFinal);
+	 		}
+	 		else
+	 		{
+	 			descuento = ((precio * 5) / 100);
+	 			
+	 			precioFinal = (precio - descuento);
+	 			console.log(precioFinal);
+	 		}
+	 	}
+	 }	
 
  	precioDescuento.value = precioFinal;
 
