9981d051ff70500e5cf979dc3f7c5cfed24c42ab
Alejandra Escubilla Texis <47353421+AleEscubillaTexis@users.noreply.github.com>
2019-02-19 12:15:36 -0300

    SWITCH (19/02)
    (clase)
    terminar el 6

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,9 +1,35 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+	var mesDelAño = document.getElementById('mes').value;
+	var mensaje;
+
+	switch (mesDelAño)
+	{
+		case "Enero":
+		case "Febrero":
+		case "Marzo":
+		case "Abril":
+		case "Mayo":
+		case "Junio":
+			mensaje = "Falta para el invierno.";
+			break;	
+
+		case "Julio":
+		case "Agosto":
+			mensaje = "Abrigate que hace frio."; 
+			break;
+		
+		case "Septiembre":
+		case "Octubre":
+		case "Noviembre":
+		case "Diciembre":
+			mensaje = "Ya pasamos el frio, ahora calor!!!.";
+			break;
+	}
+
+	alert (mensaje);
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,12 +1,29 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
-
+	var mesDelAño = document.getElementById('mes').value;
+	var mensaje;
 //alert (mesDelAño);
 	
-	
+	switch(mesDelAño)
+	{
+		case "Febrero":
+			mensaje = "tiene 28 dias";
+			break;
+		
+		case "Abril":
+		case "Junio":
+		case "Septiembre":
+		case "Noviembre":
+			mensaje = "tiene 30 dias";
+			break;
+		
+		default:
+			mensaje = "tiene 31 dias";
+			break;
+	}
 
+	alert(mensaje);
 
 
 }//FIN DE LA FUNCIÓN
