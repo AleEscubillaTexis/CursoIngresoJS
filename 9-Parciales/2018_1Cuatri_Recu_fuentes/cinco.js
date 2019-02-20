68d1848d72f08e43b8ec171b3cd95f20abab295b
Alejandra Escubilla Texis <47353421+AleEscubillaTexis@users.noreply.github.com>
2019-02-20 12:06:20 -0300

    recuperatorio (20/02)
    (clase)
    terminar

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,53 @@
 function mostrar()
 {
+	var precioHabitaciones;
+	var precio;
+	var pago;
+	var paquete;
+	var descuento;
+
+	cantidad = prompt("Ingrese la cantidad de habitaciones");
+	pago = prompt("Ingrese el tipo de pago");
+
+	precio = (precio * cantidad);
+
+	switch(pago)
+	{
+		case "visa":
+		case "mercadopago":
+			precio = ((precio * 10) / 100);
+			
+			break;
+		
+		case "paypal":
+			precio = ((precio * 15) / 100);
+			break;
+		
+		case "efectivo":
+			precio = ((precio * 20) / 100);
+			break;
+		
+		default:
+			precio = ((precio * 5) / 100);
+
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
