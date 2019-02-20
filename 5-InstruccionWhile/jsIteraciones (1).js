58fb32d50dc3c930ecc3d3fc54b0859e438c3255
Alejandra Escubilla Texis <47353421+AleEscubillaTexis@users.noreply.github.com>
2019-02-20 12:23:30 -0300

    WHILE (20/02)

--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -1,6 +1,16 @@
+//todo iteracion tiene un comienzo, un recorrido y una accion que marca su final
+
 function mostrar()
 {
-	alert('iteración while');
+	var contador = 0;
+
+	while (contador < 10)
+	{
+		contador = contador + 1;
+		console.log(contador);
+	}
+
+	//alert('iteración while');
 
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,13 @@
 function mostrar()
 {
 
-var clave = prompt("ingrese el número clave.");
+	var clave = prompt("ingrese el número clave.");
 
+	while(clave != "utn750")
+	{	
+		console.log(clave)
+		clave = prompt("ingrese el número clave.");
+
+	}
 
 }//FIN DE LA FUNCIÓN
