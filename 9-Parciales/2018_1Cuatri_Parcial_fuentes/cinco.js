e0108c2218f72dba73cc7f9bff083f7c183f88b1
AleEscubillaTexis <ale.texis@hotmail.com>
2019-02-19 22:58:58 -0300

    parcial 5 (19/02)

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,59 @@
 function mostrar()
 {
+ 	var planeta;
+ 	var mensaje;
 
+ 	planeta = prompt("Ingrese un planeta del sistema solar");
+
+ 	switch (planeta)
+ 	{
+ 		case "tierra":
+ 			mensaje = "aca vivimos";
+ 			break;
+ 		
+ 		case "mercurio":
+ 		case "venus":
+ 			mensaje = "aca hace mas calor";
+ 			break;
+ 		
+ 		case "marte":
+ 		case "jupiter":
+ 		case "saturno":
+ 		case "urano":
+ 		case "neptuno":
+ 			mensaje = "aca hace mas frio";
+ 			break;
+ 		
+ 		default:
+ 			mensaje = "no es un planeta valido";
+ 			break;
+ 	}
+
+ 	alert(mensaje);
+
+
+ 	/*if (planeta == "tierra")
+ 	{
+ 		mensaje = "aca vivimos";
+ 	}
+ 	else
+ 	{
+ 		if(planeta = "mercurio")
+ 		{
+ 			mensaje = "aca hace mas calor";
+ 		}
+ 		else
+ 		{
+ 			if(planeta = "venus")
+ 			{
+ 				mensaje = "aca hace mas calor";
+ 			}
+ 			else
+ 			{
+ 				mensaje = "aca hace mas frio";
+ 			}//error
+ 		}
+ 	}//falta ver que pasa si no es un planeta
+
+ 	alert(mensaje);*/
 }
