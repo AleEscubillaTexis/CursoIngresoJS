5a2b2b8a19d17eb05057604dbbfc1ed811fe65e2
Alejandra Escubilla Texis <47353421+AleEscubillaTexis@users.noreply.github.com>
2019-02-22 19:47:01 -0300

    recuperatorio 5 (22/02)
    (casa)
    terminado y testeado

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,41 +1,68 @@
 function mostrar()
 {
 	var precioHabitaciones;
-	var precio;
 	var pago;
 	var paquete;
 	var descuento;
+	var precioFinal;
 
-	cantidad = prompt("Ingrese la cantidad de habitaciones");
+	precioHabitaciones = prompt("Ingrese el precio por habitacion");
+	paquete = prompt("Ingrese el paquete elegido");
 	pago = prompt("Ingrese el tipo de pago");
 
-	precio = (precio * cantidad);
+	precioHabitaciones = parseInt(precioHabitaciones);
+
 
 	switch(pago)
 	{
 		case "visa":
 		case "mercadopago":
-			precio = ((precio * 10) / 100);
-			
+			descuento = 10;
 			break;
+		
 		
 		case "paypal":
-			precio = ((precio * 15) / 100);
+			
+			switch(paquete)
+			{
+				case "todoIncluido":
+					descuento = 25;
+					break;
+				
+				default:
+					descuento = 15;
+					break;
+			}
 			break;
-		
+			
 		case "efectivo":
-			precio = ((precio * 20) / 100);
+			
+			switch(paquete)
+			{
+				case "soloDesayunos":
+					descuento = 30;
+					break;
+				
+				case "todoIncluido":
+					descuento = 35;
+					break;
+				
+				default:
+					descuento = 20;
+					break;
+			}
 			break;
-		
+
 		default:
-			precio = ((precio * 5) / 100);
-
-
-
+			descuento = 5
+			break;
 
 	}
 
+	descuento = ((precioHabitaciones * descuento) / 100);
+	precioFinal = (precioHabitaciones - descuento);
 
+	alert("El precio final de la compra es $" + precioFinal);
 
 
 
