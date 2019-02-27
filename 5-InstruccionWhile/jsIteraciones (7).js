0fa107af6ef6c2d6ae86816e2089e2fd38ce0318
Alejandra Escubilla Texis <47353421+AleEscubillaTexis@users.noreply.github.com>
2019-02-27 12:01:00 -0300

    WHILE (27/02)

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -3,22 +3,28 @@ function mostrar()
 
 	var contador = 0;
 	var acumulador = 0;
-	var respuesta;
+	var respuesta = "si";
 	var numero;
 
-	while(respuesta)
+	while(respuesta != "no")
 	{
-		respuesta = confirm("¿Desea ingresar mas numeros?")
-		contador++;
-		console.log(contador);
-
 		do
 		{	
 			numero = prompt("Ingrese un numero");
 			numero = parseInt(numero);
 			acumulador = acumulador + numero;
+			console.log(acumulador);
 		}
 		while(isNaN(numero));
+		
+		respuesta = confirm("¿Desea ingresar mas numeros?")
+		if (respuesta == "no")
+		{
+			break;
+		}
+
+		contador++;
+		console.log(contador);
 	}
 	document.getElementById('suma').value=acumulador;
 	document.getElementById('promedio').value=acumulador/contador;
