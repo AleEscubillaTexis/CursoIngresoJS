7b9d8779dbffbb3f7d2f9bf69056793ee537c6df
Alejandra Escubilla Texis <47353421+AleEscubillaTexis@users.noreply.github.com>
2019-02-27 23:16:48 -0300

    recuperatorio (27/02)
    (casa)
    terminar

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
@@ -1,4 +1,45 @@
 function mostrar()
 {
+	var letra;
+	var numero;
+	var respuesta = true;
+	var contador = 0;
+	var acumulador = 0;
+	var pares;
+	var impares;
+	var ceros;
+	var promedioPositivos;
+	var sumaNegativos;
+	var maximo;
+	var minimo;
+
+
+	while(respuesta = true)
+	{
+		letra = prompt("Ingrese la letra");
+		while(isNaN(letra) == false)
+		{
+			letra = prompt("Error. Vuelva a ingresar la letra");
+		}
+
+		numero = prompt("Ingrese un numero");
+		numero = parseInt(numero);
+		while(isNaN(numero) == true || numero < -100 || numero > 100)
+		{
+			numero = prompt("Error. Vuelva a ingresar un numero");
+			numero = parseInt(numero);
+		}
+		respuesta = confirm("¿Desea seguir ingresando valores?");
+
+
+
+
+
+		contador = contador + 1
+	}
+
+
+
+
 
 }
