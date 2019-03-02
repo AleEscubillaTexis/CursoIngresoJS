c1615bc0daf44b7adfba984b48691c3023b29c22
alumno <alumno@utnfra.com.ar>
2019-03-02 10:00:43 -0300

    recuperatorio (02/03)

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,18 @@
 function mostrar()
-{
+{	
+	var precio;
+	var descuento;
+	var precioFinal;
+
+	precio = prompt("Ingresar el precio");
+	descuento = prompt("Ingresar el porcentaje de descuento");
+
+	precio = parseInt(precio);
+	descuento = parseInt(descuento);
+
+	descuento = (precio * descuento) / 100;
+	precioFinal = (precio - descuento);
+
+	elPrecioFinal.value = precioFinal;
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,18 @@
 
 function mostrar()
 {
+	var largo;
+	var ancho;
+	var perimetro;
+
+	largo = prompt("Ingrese el largo");
+	ancho = prompt("Ingrese el ancho");
+
+	largo = parseInt(largo);
+	ancho = parseInt(ancho);
+
+	perimetro = (largo + ancho) * 2;
+
+	alert("El perimetro es " + perimetro);
 
 }
