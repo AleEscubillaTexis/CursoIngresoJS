8cf6da925b0a3f0994ca88001c829dc52d9b564a
Alejandra Escubilla Texis <47353421+AleEscubillaTexis@users.noreply.github.com>
2019-03-02 13:07:36 -0300

    parcial (02/03)
    (clase)
    terminar

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,68 @@
 function mostrar()
 {
+	var marca;
+	var peso;
+	var temperatura;
+	var respuesta =  true;
+	var contadorTemperaturaPar = 0;
+	var maxPeso;
+	var maxMarca;
+	var flagPesoMaximo = false;
+	var contador = 0;
+	var acumulador = 0;
+	var promedio;
 
+
+
+	do
+	{
+		marca = prompt("Ingrese marca");
+		while(isNaN(marca) == false)
+		{
+			marca = prompt("Error. Vuelva a ingresar marca");	
+		}
+		
+		peso = prompt("Ingrese peso");
+		peso = parseInt(peso);
+		while(isNaN(peso) == true || peso < 1 || peso > 100)
+		{
+			peso = prompt("Error. Vuelva a ingresar peso");
+			peso = parseInt(peso);
+		}
+
+		temperatura = prompt("Ingrese temperatura");
+		temperatura = parseInt(temperatura);
+		while(isNaN(temperatura) == true || temperatura < -30 || temperatura > 30)
+		{
+			temperatura = prompt("Error. Vuelva a ingresar temperatura");
+			temperatura = parseInt(temperatura);
+		}
+
+
+		if (temperatura % 2 == 0)
+		{
+			contadorTemperaturaPar++;
+		}
+
+		if(flagPesoMaximo == false || peso > maxPeso)
+		{
+			maxMarca = marca;
+			maxPeso = peso;
+			flagPesoMaximo = true;
+		}
+
+		if (temperatura > 0)
+		{
+			contadorTempMenosCero++;
+		}
+		contadorPeso++:
+		acumuladorPeso = acumuladorPeso + peso;
+
+		respuesta = confirm("¿Desea ingresar mas productos?");
+	}	
+	while(respuesta == true);
+	promedio = acumuladorPeso + 
+	document.write("Cantidad de temperaturas pares " + contadorTemperaturaPar);
+	
+	
 }
